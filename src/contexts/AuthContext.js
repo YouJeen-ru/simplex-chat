2a33f9d4fb5036ff3fc7f3729c1b20a8952696c5
YouJeen-ru <1.rouch@mail.ru>
2021-10-14 15:07:20 +0300

    fix misprint useHistory

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -10,7 +10,7 @@ export  const useAuth = () => useContext(AuthContext)
 export const AuthProvider = ({ children }) => {
     const [loading, setLoading] = useState(true)
     const [user, setUser] = useState({})
-    const [history, setHistory] = useHistory()
+    const history = useHistory()
 
     useEffect(() => {
         auth.onAuthStateChanged((user) => {
